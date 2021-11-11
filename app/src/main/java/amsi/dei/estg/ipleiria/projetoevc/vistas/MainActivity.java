5f1b0109ab74df9fde76f3d16f67a167ea5885a0
Joao Pereira <joao.pereira@onesource.pt>
2021-11-11 17:24:17 +0000

    pastas projeto

--- app/src/main/java/amsi/dei/estg/ipleiria/projetoevc/vistas/MainActivity.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/projetoevc/vistas/MainActivity.java
@@ -1,8 +1,10 @@
-package amsi.dei.estg.ipleiria.projetoevc;
+package amsi.dei.estg.ipleiria.projetoevc.vistas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import amsi.dei.estg.ipleiria.projetoevc.R;
 
 public class MainActivity extends AppCompatActivity {
 
