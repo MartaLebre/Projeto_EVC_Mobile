99539c925c8c9d54f487f775ea9e278d2dece54b
Joao Pereira <joao.pereira@onesource.pt>
2021-11-16 18:09:39 +0000

    app bar

--- app/src/main/java/amsi/dei/estg/ipleiria/projetoevc/vistas/MenuMainActivity.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/projetoevc/vistas/MenuMainActivity.java
@@ -1,14 +1,22 @@
 package amsi.dei.estg.ipleiria.projetoevc.vistas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
+import amsi.dei.estg.ipleiria.projetoevc.R;
+
 public class MenuMainActivity extends AppCompatActivity {
 
+
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_main);
+        toolbar.findViewById(R.id.myToolBar);
+
+        setSupportActionBar(toolbar);
     }
 }
