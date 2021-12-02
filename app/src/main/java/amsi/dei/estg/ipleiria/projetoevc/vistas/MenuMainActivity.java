6fe16aaebf635937801f499e111a02ced36e5599
Joao Pereira <joao.pereira@onesource.pt>
2021-12-02 18:45:33 +0000

    clean

--- app/src/main/java/amsi/dei/estg/ipleiria/projetoevc/vistas/MenuMainActivity.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/projetoevc/vistas/MenuMainActivity.java
@@ -1,6 +1,9 @@
 package amsi.dei.estg.ipleiria.projetoevc.vistas;
 
 
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.navigation.NavigationView;
@@ -8,4 +11,8 @@ import com.google.android.material.navigation.NavigationView;
 public class MenuMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
 }
